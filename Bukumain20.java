8158888c8f30953a6144379175c30f2ca8b5c15b
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-02-23 14:40:05 +0700

    Percobaan 2

---
+++ Bukumain20.java
@@ -0,0 +1,16 @@
+public class Bukumain20 {
+    public static void main(String[] args) {
+
+        Buku20 bk1 = new Buku20();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiharga(60000);
+        bk1.tampilInformasi();
+    }
+}
