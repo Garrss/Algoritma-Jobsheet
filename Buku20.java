1e99574d6a7d0ab728ceb1259c1a02a8f202b83d
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-02-27 23:37:48 +0700

    tambahan

--- Buku20.java
+++ Buku20.java
@@ -24,6 +24,22 @@ public class Buku20 {
         harga = hrg;
     }
 
+    int hitungHargaTotal(int jml) {
+        return harga * jml;
+    }
+
+    int hitungDiskon(int jml) {
+        int diskon = 0;
+        int hargaTotal = hitungHargaTotal(jml);
+        if (hargaTotal >= 150000) {
+            diskon = (int) (0.12 * hargaTotal);
+        } else if (hargaTotal < 150000 && hargaTotal >= 75000)
+            diskon = (int) (0.05 * hargaTotal);
+        else
+            diskon = 0;
+        return diskon;
+    }
+
     public Buku20() {
 
     }
