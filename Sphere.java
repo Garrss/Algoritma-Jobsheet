df6c2af10197768d78f93d55c633f0a6f8124359
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-03-29 00:03:13 +0700

    assigment no 1

---
+++ Sphere.java
@@ -0,0 +1,16 @@
+public class Sphere {
+    int radius;
+
+    Sphere(int radius) {
+        this.radius = radius;
+    }
+
+    double calculateArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+
+    double calculateVolume() {
+        return (4 * Math.PI * Math.pow(radius, 3)) / 3;
+
+    }
+}
---
+++ squarePyramid.java
@@ -0,0 +1,17 @@
+public class squarePyramid {
+    int side, height;
+
+    squarePyramid(int a, int b) {
+        side = a;
+        height = b;
+    }
+
+    int calculateArea() {
+        return side * side;
+    }
+
+    double calculateVolume(int area) {
+        return area * height / 3;
+
+    }
+}
