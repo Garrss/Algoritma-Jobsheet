42f4c0154322744d19e43a0eec7ca6d97c4a5959
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-04-01 14:49:18 +0700

    Jobsheet 5

---
+++ MainFaktorial.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class MainFaktorial {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("===================================================");
+        System.out.print("Input the number of elemente you want to count : ");
+
+        int elemen = sc.nextInt();
+
+        Faktorial[] fk = new Faktorial[elemen];
+        for (int i = 0; i < elemen; i++) {
+            fk[i] = new Faktorial();
+            System.out.print("Input the data value to-" + (i + 1) + " : ");
+            fk[i].num = sc.nextInt();
+        }
+        System.out.println("==================================================");
+        System.out.println("Factorial Results with Brute Force");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Factorial of Value " + fk[i].num + " is : " + fk[i].FaktorialBF(fk[i].num));
+        }
+        System.out.println("==================================================");
+        System.out.println("Factorial Results with Divide and Conquer");
+        for (int i = 0; i < elemen; i++) {
+            System.out.println("Factorial of value " + fk[i].num + " is : " + fk[i].FaktorialDC(fk[i].num));
+        }
+        System.out.println("==================================================");
+    }
+}
