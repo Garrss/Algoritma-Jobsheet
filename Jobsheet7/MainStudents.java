4bf06f371f91defb49378fac7acdd728b9d6644f
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-04-07 23:27:30 +0700

    Jobsheet 7

---
+++ Jobsheet7/MainStudents.java
@@ -0,0 +1,46 @@
+package Jobsheet7;
+
+import java.util.Scanner;
+
+public class MainStudents {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        Scanner sl = new Scanner(System.in);
+
+        SearchStudents data = new SearchStudents();
+        int amountStudent = 5;
+
+        System.out.println("==================================");
+        System.out.println("Input student data accordingly from smallest NIM");
+        for (int i = 0; i < amountStudent; i++) {
+            System.out.println("---------");
+            System.out.print("NIM\t:");
+            int nim = s.nextInt();
+            System.out.print("Name\t:");
+            String name = sl.nextLine();
+            System.out.print("Age\t:");
+            int age = s.nextInt();
+            System.out.print("GPA\t:");
+            double gpa = s.nextDouble();
+
+            Students std = new Students(nim, name, age, gpa);
+            data.add(std);
+        }
+
+        System.out.println("==================================");
+        System.out.println("Entire Student Data");
+        data.display();
+
+        System.out.println("_______________________");
+        System.out.println("_______________________");
+        System.out.print("Search student by NIM: ");
+        int Search = s.nextInt();
+        System.out.println("Using Sequential Search");
+        int position = data.FindBinarySearch(Search, 0, amountStudent - 1);
+
+        data.showPosition(Search, position);
+
+        data.showData(Search, position);
+    }
+}
---
+++ Jobsheet7/SearchStudents.java
@@ -0,0 +1,67 @@
+package Jobsheet7;
+
+public class SearchStudents {
+    Students[] listStd = new Students[5];
+    int idx = 0;
+
+    public void add(Students std) {
+        if (idx < listStd.length) {
+            listStd[idx] = std;
+            idx++;
+        } else {
+            System.out.println("Data is already full");
+        }
+    }
+
+    public void display() {
+        for (Students students : listStd) {
+            students.display();
+            System.out.println("=================================");
+        }
+    }
+
+    public int FindBinarySearch(int Search, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (Search == listStd[mid].nim) {
+                return (mid);
+            } else if (listStd[mid].nim < Search) {
+                return FindBinarySearch(Search, left, mid - 1);
+            } else {
+                return FindBinarySearch(Search, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
+    public int findSeqSearch(int Search) {
+        int position = -1;
+        for (int i = 0; i < listStd.length; i++) {
+            if (listStd[i].nim == Search) {
+                position = i;
+                break;
+            }
+        }
+        return position;
+    }
+
+    public void showPosition(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Data : " + x + " is found in index-" + pos);
+        } else {
+            System.out.println("Data :  " + x + " is not found");
+        }
+    }
+
+    public void showData(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("NIM \t : " + x);
+            System.out.println("Name \t : " + listStd[pos].name);
+            System.out.println("Age \t : " + listStd[pos].age);
+            System.out.println("IPK \t : " + listStd[pos].gpa);
+        } else {
+            System.out.println("Data " + x + "is not found");
+        }
+    }
+}
