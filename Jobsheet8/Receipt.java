8123ca2c65d8658fb58fa1c0b31e22f8ce670e17
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-05-19 15:15:29 +0700

    Assignment 2

---
+++ Jobsheet8/Receipt.java
@@ -0,0 +1,12 @@
+public class Receipt {
+    int Id, quantity, price;
+    String date;
+
+    Receipt(int Id, int quantity, int price, String date) {
+        this.Id = Id;
+        this.quantity = quantity;
+        this.price = price;
+        this.date = date;
+    }
+
+}
