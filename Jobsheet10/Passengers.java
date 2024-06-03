91b76fed88c02ba49bc888575aaf0e6968b11664
Muhammad Tegar Hibatulloh <143469879+Garrss@users.noreply.github.com>
2024-06-03 21:58:52 +0700

    Passengers

---
+++ Jobsheet10/Passengers.java
@@ -0,0 +1,15 @@
+package Jobsheet10;
+
+public class Passengers {
+    String name, cityOrigin, cityDestination;
+    int ticketAmount, price;
+
+    public Passengers(String nm, String co, String cd, int ta, int pr) {
+        name = nm;
+        cityOrigin = co;
+        cityDestination = cd;
+        ticketAmount = ta;
+        price = pr;
+    }
+
+}
